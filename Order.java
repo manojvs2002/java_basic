a6a77d601e6f999ecf8f324909afe8dd27dfeefd
manojvs2002 <106401589+manojvs2002@users.noreply.github.com>
2022-12-26 20:34:30 +0530

    Add files via upload

---
+++ Order.java
@@ -0,0 +1,34 @@
+package arrays;
+
+import java.util.*;
+
+public class Order {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+			Scanner in=new Scanner(System.in);
+			String[] ls=new String[5];
+			System.out.println("Enter five strings : ");
+			System.out.print("First : ");
+			ls[0]=in.next();
+			System.out.print("Second : ");
+			ls[1]=in.next();
+			System.out.print("Third : ");
+			ls[2]=in.next();
+			System.out.print("Fourth : ");
+			ls[3]=in.next();
+			System.out.print("Fifth : ");
+			ls[4]=in.next();
+			Arrays.sort(ls);
+			for(int i=0;i<5;i++)
+			{
+				System.out.println(ls[i]);
+			}
+		}
+
+
+	
+
+}
